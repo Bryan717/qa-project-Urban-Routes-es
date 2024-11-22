964ccd626596ea154ddeb19ae519943252e2c597
Bryan Guillermo <bryanca717@gmail.com>
2024-11-21 19:05:12 -0800

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,12 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+# URL de la aplicación Urban Routes
+urban_routes_url = 'https://cnt-2aa217ba-95d4-4888-a1e2-a1fb76b0d5a0.containerhub.tripleten-services.com?lng=es'
+# Direcciones para el origen y el destino de la prueba
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
+# Información de contacto para las pruebas de autenticación
 phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
+# Información de tarjeta de crédito para las pruebas de pago
+card_number = '1234 5678 9100'
+card_code = '111'
+# Mensaje de prueba para el conductor
+message_for_driver = 'Llevame al museo'
